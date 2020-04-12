eb96726c6a29c420b7aa8a77c5b6a813800cda87
kylebyrne <kyletbyrne96@gmail.com>
2020-04-12 11:28:34 +0100

    Bringing in more config and getting symlinks working

--- link.sh
+++ link.sh
@@ -8,6 +8,7 @@ linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
 for file in $linkables ; do
     target="$HOME/.$( basename "$file" '.symlink' )"
     if [ -e "$target" ]; then
+        echo " Target is $target"
         echo "~${target#$HOME} already exists... Skipping."
     else
         echo "Creating symlink for $file"
